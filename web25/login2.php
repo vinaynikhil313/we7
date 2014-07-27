fca9a23e474a3866ab35848aae17477038e5e63e
sushmakandru <sushmakandru24@gmail.com>
2014-07-27 11:13:39 +0530

    mainnn projecttt

---
+++ web25/login2.php
@@ -0,0 +1,38 @@
+<?php
+session_start();
+//   $con=mysqli_connect("localhost","root","cfg2014!","we7");
+   // $con=mysqli_connect("http://ec2-54-254-195-240.ap-southeast-1.compute.amazonaws.com","root","cfg2014!","we7");
+        include 'connect.php';
+    $phpro_username=$_POST['U_ID'];
+    $phpro_password=$_POST['Password'];
+    $phpro_role=$_POST['Role'];
+     
+		
+        $sql="SELECT Role,U_ID,Password FROM Login WHERE U_ID ='$phpro_username' AND Password ='$phpro_password' AND Role ='$phpro_role'";
+		$_SESSION['userid']=0;
+		
+		
+
+			$result=mysqli_query($con,$sql);
+			echo "ABCD\n";
+			if(!$result)
+			{
+				//header("Location: display_error.php");
+			}
+			//echo "EFGH\n";
+			$row=mysqli_fetch_array($result);
+			if (($row['U_ID'])!=NULL)
+  			{
+  				$_SESSION['userid']=$row['U_ID'];
+                        $x=$_SESSION['userid'];
+				
+				if($phpro_role == 'Teacher')
+					header("Location: teacher_first.php");
+				else if($phpro_role == 'CC')
+					header("Location: CC_first1.php");
+				else if($phpro_role == 'Trainer')
+					header("Location: trainer_first.php");
+			}
+		
+
+?>
---
+++ web25/trainer_forth.php
@@ -0,0 +1,179 @@
+<?php session_start();?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Simple Sidebar - Start Bootstrap Template</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/simple-sidebar.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+	<div class="header_bg">
+					<div class="wrap">
+						<div class="header">
+							
+							<div class="logo">
+								<a href="index.php"><img src="images/logo.png" alt="" style=""/></a>
+							</div>	
+														<div class="btn-group" style="float:right;">
+  <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
+    Logout<span class="caret"></span>
+  </button>
+  <ul class="dropdown-menu" role="menu">
+    <li><a href="logout.php">Logout</a></li>
+  
+</div>
+						</div>
+					</div>
+	</div>
+
+    <div id="wrapper">
+
+        <!-- Sidebar -->
+        <div id="sidebar-wrapper">
+        					
+            <ul class="sidebar-nav">
+                <li class="sidebar-brand">
+                    <a href="#">
+                        Menu
+                    </a>
+                </li>
+                <li>
+                    <a href="trainer_second.php">View Teachers of the School </a>
+                </li>
+                <li>
+                    <a href="trainer_third.php">Create an Observation </a>
+                </li>
+                <li>
+                    <a href="trainer_forth.php">Update attendance </a>
+                </li>
+            </ul>
+        </div>
+        <!-- /#sidebar-wrapper -->
+
+        <!-- Page Content -->
+
+
+
+
+   <?php
+        	
+			include 'connect.php';
+			if(isset($_POST['mod_att']))
+			{
+				$teacher_name=$_POST['teachername'];
+				$session_name=$_POST['sessionname'];
+				
+				$sql="UPDATE Training_Details SET Attendance_Count=Attendance_Count+1 WHERE Session_Name='$session_name' AND 
+				Teacher_ID IN (Select Teacher_ID FROM Teacher_Profile Where Teacher_Name='$teacher_name')";
+				$result = mysqli_query($con, $sql);
+				if($result)
+				{
+					echo "Attendance Updated Successfully\n";
+				}
+				else
+				{
+					echo "Attendance not Updated\n";
+				}
+			}
+			
+        ?>
+
+        <div id="page-content-wrapper">
+            <div class="container-fluid">
+            		<div class="logo">
+								<img src="web/images/logo.png" alt="" />
+							</div>	
+                <div class="row">
+                    <div class="col-lg-12">
+                       
+                        
+
+                        <a href="#menu-toggle" class="btn btn-default" id="menu-toggle">Toggle Menu</a>
+                    
+                         <h3>Please enter the school name </h3>
+                     		
+
+						<form method="post" action="trainer_forth.php">
+						
+						<div class="panel-body">
+    					<div class="table-responsive">
+  							<table class="table">
+   								<tr>
+   									<td colspan="1"><span>Teacher Name </span></td>
+   									<td colspan="3"><span><input type="text" name="teachername"/></span></td>
+   								</tr>
+   								<tr>
+   									<td colspan="1"><span>Session Name </span></td>
+   									<td colspan="3"><span><input type="text" name="sessionname"/></span></td>
+   								</tr>
+   										
+   							
+  							</table>
+						</div>
+
+					
+				
+					</div>
+
+
+			
+							<input type="submit" name="mod_att" value="Modify Attendance"/>
+
+					</form>
+			
+
+
+
+					
+    			</div>
+			</div>
+			</div>              
+                  
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- /#page-content-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+
+    <!-- jQuery Version 1.11.0 -->
+    <script src="js/jquery-1.11.0.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Menu Toggle Script -->
+    <script>
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
+    </script>
+
+</body>
+
+</html>
+
